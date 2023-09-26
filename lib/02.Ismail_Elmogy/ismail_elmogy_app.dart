7c3b6d670e5c5cf5d4e7f2793bb26570585318f2
AyA_Abd_Elmoneim2 <ayaabdelmon@gmail.com>
2023-09-26 11:50:30 +0000

    🚩 02.Ismail_Elmogy 👉 Add Structure

---
+++ lib/02.Ismail_Elmogy/ismail_elmogy_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_courses/02.Ismail_Elmogy/test_screen.dart';
+
+class IsmailElmogyApp extends StatelessWidget {
+  const IsmailElmogyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Ismail_Elmogy App',
+      home: TestScreen(),
+    );
+  }
+}
