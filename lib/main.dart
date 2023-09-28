aa7d43c7521bffc74f5642cc842ec355a302a1a5
AyA_Abd_Elmoneim2 <ayaabdelmon@gmail.com>
2023-09-28 02:02:50 +0000

    🚩 02.Ismail_Elmogy 👉 Add Injector

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_courses/02.Ismail_Elmogy/ismail_elmogy_app.dart';
+import 'package:flutter_courses/02.Ismail_Elmogy/clean_architecture/quote_app/injector.dart'
+    as di;
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const IsmailElmogyApp();
